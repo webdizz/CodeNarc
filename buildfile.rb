a004a4914b2c11173f8479223f10cbe2c1d042aa
Izzet Mustsfaiev <webdizz@gmail.com>
2013-10-22 01:52:27 +0300

    Compilation tuning

--- buildfile.rb
+++ buildfile.rb
@@ -1,10 +1,11 @@
 require 'buildr/groovy'
-Buildr::Groovy::Groovyc::REQUIRES.groovy = '1.7.5'
+groovyVersion = '1.7.5'
+Buildr::Groovy::Groovyc::REQUIRES.groovy = groovyVersion
 repositories.remote << 'http://repo1.maven.org/maven2'
 
 define 'codenarc' do
   project.group = 'org.codenarc'
   project.version = '0.19'
-  compile.from('src/main/java').with 'log4j:log4j:jar:1.2.13', 'org.gmetrics:GMetrics:jar:0.5', 'org.codehaus.groovy:groovy-all:jar:1.7.5'
+  compile.from('src/main/java').using(:source=>'1.5').with 'log4j:log4j:jar:1.2.13', 'org.gmetrics:GMetrics:jar:0.5', "org.codehaus.groovy:groovy-all:jar:#{groovyVersion}", 'org.apache.ant:ant:jar:1.7.1'
   compile.from('src/main/groovy').using(:groovyc).with 'junit:junit:jar:4.8.2'
 end
