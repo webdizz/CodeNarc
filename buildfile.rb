25605e4b3bf0c7342aca819d74b21a9879208695
Izzet Mustafaiev <webdizz@gmail.com>
2013-10-22 09:42:00 +0300

    Packaging was added

--- buildfile.rb
+++ buildfile.rb
@@ -8,4 +8,5 @@ define 'codenarc' do
   project.version = '0.19'
   compile.from('src/main/java').with 'log4j:log4j:jar:1.2.13', 'org.gmetrics:GMetrics:jar:0.5', "org.codehaus.groovy:groovy-all:jar:#{groovyVersion}"
   compile.from('src/main/groovy').using(:groovyc).with 'junit:junit:jar:4.8.2'
+  package :jar
 end
